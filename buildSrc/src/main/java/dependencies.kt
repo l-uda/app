704161b465d414536ff45f5819a3d16c716c26bd
Nicolò Balzarotti <nicolo.balzarotti@iit.it>
2022-09-12 10:37:50 +0200

    bump version to 1.1.3.1

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -5,7 +5,7 @@ object Configs {
     const val minSdkVersion     = 26
     const val targetSdkVersion  = 26
     const val versionCode       = 11
-    const val versionName       = "1.0.1.11"
+    const val versionName       = "1.1.3.1"
 }
 
 object Versions {
